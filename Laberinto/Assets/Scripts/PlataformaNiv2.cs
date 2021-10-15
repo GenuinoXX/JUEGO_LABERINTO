2bd332307f0f8c42c31cae066a347617b6d6d69f
GenuinoXX <maurisebas2000@gmail.com>
2021-10-15 20:39:43 +0200

    Nivel 2(parte1)

---
+++ Laberinto/Assets/Scripts/PlataformaNiv2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlataformaNiv2 : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float fuerza;
+    float posY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float desp1H = Input.GetAxis("Horizontal");
+        float desp1V = Input.GetAxis("Vertical");
+
+        rb.AddTorque(new Vector3(0f, 0f, 0.1f) * desp1V * fuerza);
+        rb.AddTorque(new Vector3(0.1f, 0f, 0f) * desp1H * fuerza);
+    }
+}
